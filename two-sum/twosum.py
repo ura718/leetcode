a7560516948943536edc1067f0a53454fc3fb7c2
Yuri M <ura718@gmail.com>
2022-01-12 10:52:50 -0500

    na

--- two-sum/twosum.py
+++ two-sum/twosum.py
@@ -13,19 +13,23 @@ def calculate(nums):
 
   data={}                                 # declare empty dict
   for index, value in enumerate(nums):    # loop over elements and use enumerate to index values
-    if target - value in data:            # if result of target and value exist in data do the following
-      print target, value, target-value, data[target-value]
-      print ([data[target-value]])
-      return [data[target-value], index]  # if value is found then return value,index
+    diff = target - value                 # assign result of target - value to diff
+
+    # if diff exist in data dict return key,value initially data will be empty in data 
+    # but we will add to it in else statement. Added format is key=value, value=index
+    # so its technically reversed when you reference data using key you get index as output.
+
+    if diff in data:                      
+      return [data[diff], index]          # if value is found then return value,index
     else:                                 # else if result of target and value does not exist,
       data[value] = index                 # add value,index to data
-
 
 
     
 nums=[2,7,11,15]                 # list index: 0,1,2,3
 target=9                              
 calculate(nums)                  # output should be: [0,1]
+
 
 nums=[3,2,4]                     # list index: 0,1,2   
 target=6      
@@ -34,3 +38,4 @@ calculate(nums)                  # output should be: [1,2]
 nums=[3,3]                       # list index: 0,1
 target=6      
 calculate(nums)                  # output should be: [0,1]
+
